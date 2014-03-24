700276b8eb2ed10e7b643bd21ef06988414391d2
Dan Tao <daniel.tao@gmail.com>
2014-03-24 07:29:44 -0700

    never mind that gulp silliness (overkill)

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    markdown = require('gulp-markdown');
-
-gulp.task('default', function () {
-    gulp.src('src/*.markdown')
-        .pipe(markdown())
-        .pipe(gulp.dest('build'));
-});
